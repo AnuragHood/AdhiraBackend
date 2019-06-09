71fa2087f0a7ef712ba0d454069d5c1e94050ea7
AnuragHood <pathak.sn.anurag@gmail.com>
2019-06-09 11:43:50 -0700

    First commit

---
+++ src/main/java/edu/adhira/adhira/AdhiraApplication.java
@@ -0,0 +1,13 @@
+package edu.adhira.adhira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhiraApplication.class, args);
+	}
+
+}
